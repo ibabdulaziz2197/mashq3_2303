dadc6a9369adf9c2431ce704d7504f2f514e538e
Abdulaziz <ibabdulaziz2197@gmail.com>
2026-03-23 16:06:16 +0500

    Implement functions to modify global variables
    Added multiple functions to modify global variables and print their values.

---
+++ main.py
@@ -0,0 +1,94 @@
+# 10
+temperature = 20
+
+def ozgartr():
+    global temperature
+    temperature = 22
+
+ozgartr()
+print(temperature)
+
+11
+temperature = 20
+
+def ozgartr():
+    global temperature
+    temperature = 22
+
+ozgartr()
+print(temperature)
+
+# 12
+product_count = 0
+
+def oshrirsn():
+    global product_count
+    product_count += 1
+
+oshrirsn()
+print(product_count)
+
+
+13
+flag = False = False
+
+
+def ozgartr():
+    global is_active
+    flag = False = "True"
+
+ozgartr()
+print(is_active)
+
+14
+flag = False = False
+
+
+def ozgartr():
+    global is_active
+    flag = False = "True"
+
+ozgartr()
+print(is_active)
+
+
+
+# 15
+score = 0
+
+def qoshsn():
+    global score
+    score += 5
+
+qoshsn()
+print(score)
+
+# 16
+score = 100
+
+def kamaytir():
+    global score
+    score = score - 10
+
+kamaytir()
+print(score)
+
+# 19
+password = 1234
+
+def ozgartr():
+    global password
+    password = 2552
+
+ozgartr()
+print(password)
+
+# 20
+log = []
+
+def funk():
+    global log
+    log = 11
+
+funk()
+print(funk())
